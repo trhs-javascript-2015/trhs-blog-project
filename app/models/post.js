42942b21ffafdad21f08a853de45401ead454f8e
Kevin Partington <platinum.azure@kernelpanicstudios.com>
2015-07-09 05:09:49 +0000

    Adding post model. Adding post FixtureAdaptor. Adding index route which will currently render all posts.

--- app/models/post.js
+++ app/models/post.js
@@ -1,7 +1,16 @@
 import DS from 'ember-data';
 
-export default DS.Model.extend({
+let Post = DS.Model.extend({
   title: DS.attr('string'),
   author: DS.attr('string'),
   content: DS.attr('string')
 });
+
+Post.reopenClass({
+  FIXTURES: [
+    { id: 1, title: 'My First Post', author: 'Kevin', content: "This is a post.\n\n*Isn't it awesome?*" },
+    { id: 2, title: 'Another Post', author: 'Kevin', content: "This is another post." }
+  ]
+});
+
+export default Post;
